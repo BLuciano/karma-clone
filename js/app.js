100ce6129171ccb0932e56f76edfa1b163dbb97e
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2016-04-05 03:23:35 -0700

    Refactored nav menu toggle

--- js/app.js
+++ js/app.js
@@ -3,23 +3,15 @@ window.onload = function(){
 	var links = navMenu.getElementsByTagName("a");
 	var dropDown = document.getElementById("drop-down");
 	
-	/*Set display to none for first click to fire correctly*/
-	navMenu.style.display = "none";
-	
 	//toggle drop down menu
 	dropDown.onclick = function(){
-		if(navMenu.style.display === "none"){
-			navMenu.style.display = "block";
-		}
-		else{
-			navMenu.style.display = "none";
-		}
+		navMenu.classList.toggle("showNav");
 	};
 
 	//set nav menu links to hide menu when clicked
 	for(var i = 0; i < links.length; i++){
 		links[i].onclick = function(){
-			navMenu.style.display = "none";
+			navMenu.classList.remove("showNav");
 		}
 	}
 };
